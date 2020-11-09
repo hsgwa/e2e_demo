e5c0921cd174e48a4fb352ba3f0e73d8c1ab7e60
hsgwa <hasegawa@isp.co.jp>
2020-11-09 11:42:40 +0900

    add: trace point

--- src/sensor_dummy.cpp
+++ src/sensor_dummy.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include <memory>
 
+#include "communication_trace/comm_tracer.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
 
@@ -14,26 +15,43 @@ public:
     get_parameter<int>("period_ms", period_ms);
     auto period = std::chrono::milliseconds(period_ms);
 
+
     auto callback1 = [&]() {
       auto msg = std::make_unique<sensor_msgs::msg::Image>();
       msg->header.stamp = now();
+
+      pub1_tracer_->publish(msg->header);
       pub1_->publish(std::move(msg));
     };
+
     auto callback2 = [&]() {
       auto msg = std::make_unique<sensor_msgs::msg::Image>();
       msg->header.stamp = now();
+
+      auto msg_trace = std::make_unique<communication_trace_msgs::msg::CommunicationTrace>();
+      msg_trace->header.stamp = msg->header.stamp;
+      msg_trace->stamp = now();
+
+      pub2_tracer_->publish(msg->header);
       pub2_->publish(std::move(msg));
     };
+
     pub1_ = create_publisher<sensor_msgs::msg::Image>("input1", 1);
+    pub1_tracer_ = std::make_shared<CommTracer>(get_name(), pub1_->get_topic_name(), "_pub");
     timer1_ = create_wall_timer(period, callback1);
+
     pub2_ = create_publisher<sensor_msgs::msg::Image>("input2", 1);
+    pub2_tracer_ = std::make_shared<CommTracer>(get_name(), pub2_->get_topic_name(), "_pub");
     timer2_ = create_wall_timer(period, callback2);
   }
 
 private:
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub1_;
+  std::shared_ptr<CommTracer> pub1_tracer_;
   rclcpp::TimerBase::SharedPtr timer1_;
+
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub2_;
+  std::shared_ptr<CommTracer> pub2_tracer_;
   rclcpp::TimerBase::SharedPtr timer2_;
 };
 
