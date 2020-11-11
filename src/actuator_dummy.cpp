ca8005dfadce8476f956b5779cd48797cb7320b3
hsgwa <hasegawa@isp.co.jp>
2020-11-11 17:38:26 +0900

    add: trace point

--- src/actuator_dummy.cpp
+++ src/actuator_dummy.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include <memory>
 
+#include "communication_trace/comm_tracer.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
 
@@ -9,9 +10,14 @@ using namespace std::chrono_literals;
 class ActuatorDummy : public rclcpp::Node {
 public:
   ActuatorDummy() : Node("actuator_dummy_node") {
-    auto callback1 = [&](sensor_msgs::msg::Image::UniquePtr msg) {(void)msg;};
+    auto callback1 = [&](sensor_msgs::msg::Image::UniquePtr msg) {
+      (void)msg;
+      pub1_tracer_->publish(msg->header);
+    };
 
-    auto callback2 = [&](sensor_msgs::msg::Image::UniquePtr msg) {(void)msg;};
+    auto callback2 = [&](sensor_msgs::msg::Image::UniquePtr msg) {
+      pub2_tracer_->publish(msg->header);
+    };
 
     sub1_ = create_subscription<sensor_msgs::msg::Image>("input1", 1, callback1);
     sub2_ = create_subscription<sensor_msgs::msg::Image>("input2", 1, callback2);
@@ -20,7 +26,9 @@ public:
 
 private:
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub1_;
+  std::shared_ptr<CommTracer> pub1_tracer_;
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub2_;
+  std::shared_ptr<CommTracer> pub2_tracer_;
 };
 
 int main(int argc, char *argv[]) {
