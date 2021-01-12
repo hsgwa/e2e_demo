eb2f2c65add0d0315c61b0c801f2837ab9e16adb
hsgwa <hasegawa@isp.co.jp>
2021-01-12 23:42:29 +0900

    add lttng to launch file

--- launch/demo.py
+++ launch/demo.py
@@ -2,6 +2,8 @@ import launch
 import launch.actions
 import launch.substitutions
 import launch_ros.actions
+from tracetools_launch.action import Trace
+
 
 def generate_launch_description():
     period1_ns = int(1.0/11.0*1.0e9)
@@ -10,7 +12,11 @@ def generate_launch_description():
     callback_duration_ns = int(0.01*1.0e9)
 
     return launch.LaunchDescription([
-        launch_ros.actions.Node(
+        Trace(
+            session_name='e2e_demo',
+            events_kernel=[]
+        ),
+       launch_ros.actions.Node(
             package='e2e_demo', executable='sensor_dummy', output='screen',
             remappings=[
                 ('input1', 'topic1'),
